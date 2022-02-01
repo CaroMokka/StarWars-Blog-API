99620429c440f4007a820413374107a51fbf48f8
Carolina Araya <ci.arayagonzalez@gmail.com>
2022-02-01 02:13:46 +0000

    modelando sqlAlchemy

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Characters, Planets, Starships
 #from models import Person
 
 app = Flask(__name__)
@@ -30,14 +30,103 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+#GET ALL USERS
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_users():
+    users = User.query.all()
+    all_users = list(map(lambda x: x.serialize(), users))
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    return jsonify(all_users), 200
 
-    return jsonify(response_body), 200
+#POST USER
+@app.route('/user', methods=['POST'])
+def create_user():
+    request_body_user = request.get_json()
+    user = User(user_name=request_body_user["user_name"], first_name=request_body_user["first_name"], last_name=request_body_user["last_name"], email=request_body_user["email"], password=request_body_user["password"])
+    db.session.add(user)
+    db.session.commit() 
+    return jsonify("successfully registered"), 200 
+
+#PUT USER 
+@app.route('/user/<int:id>', methods=['PUT'])
+def update_user(id):
+    request_body_user = request.get_json()
+    user = User.query.get(id)
+    if user is None:
+       raise APIException('User not found', status_code=404)
+
+    if "user_name" in request_body_user:
+        user.user_name = request_body_user["user_name"]
+    if "email" in request_body_user:
+        user.email = request_body_user["email"]
+    db.session.commit()
+    
+    return jsonify("user updated successfully"), 200 
+
+#DELETE USER
+@app.route('/user/<int:id>', methods=['DELETE'])
+def delete_user(id):
+    user = User.query.get(id)
+    if user is None:
+       raise APIException('User not found', status_code=404)
+    db.session.delete(user)
+    db.session.commit()
+  
+    
+    return jsonify("Deleted user"), 200         
+
+
+#GET ALL PLANETS >>>>>>>>>>>>>>>>>>>>>>>>>
+@app.route('/planets', methods=['GET'])
+def get_planets():
+    planets = Planets.query.all()
+    all_planets = list(map(lambda x: x.serialize(), planets))
+   
+    return jsonify(all_planets), 200
+
+
+#POST PLANET >>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+@app.route('/planets', methods=['POST'])
+def create_planet():
+    request_body_planets = request.get_json()
+    planet = Planets(name=request_body_planets["name"], diameter=request_body_planets["diameter"], climate=request_body_planets["climate"], terrain=request_body_planets["terrain"], population=request_body_planets["population"])
+    db.session.add(planet)
+    db.session.commit() 
+    return jsonify("Planet successfully registered"), 200    
+
+
+#PUT PLANET >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+@app.route('/planets/<int:id>', methods=['PUT'])
+def update_planet(id):
+    request_body_planet = request.get_json()
+    planet = Planets.query.get(id)
+    if planet is None:
+       raise APIException('Planet not found', status_code=404)
+
+    if "name" in request_body_planet:
+        planet.name = request_body_planet["name"]
+    
+    db.session.commit()
+    
+    return jsonify("user updated successfully"), 200
+
+
+
+#DELETE PLANET >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+@app.route('/planets/<int:id>', methods=['DELETE'])
+def delete_planet(id):
+    planet = Planet.query.get(id)
+    if planet is None:
+       raise APIException('Planet not found', status_code=404)
+    db.session.delete(planet)
+    db.session.commit()
+  
+    
+    return jsonify("Deleted planet"), 200 
+
+
+
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,18 +2,92 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+#TABLA USER
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(80), unique=True, nullable=False)
+    first_name = db.Column(db.String(80), nullable=False)
+    last_name = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=True, default=True)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.user_name
 
     def serialize(self):
         return {
             "id": self.id,
+            "user_name": self.user_name,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Characters(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    height = db.Column(db.Integer, nullable=False)
+    gender = db.Column(db.String(30), nullable=False)
+    homeworld = db.Column(db.String(30), nullable=False)
+
+    def __repr__(self):
+        return '<Characters %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "height": self.height,
+            "gender": self.gender,
+            "homeworld": self.homeworld,
+            # do not serialize the password, its a security breach
+        }    
+
+
+#TABLA PLANETAS
+class Planets(db.Model):  
+    id = db.Column(db.Integer, primary_key=True) 
+    name = db.Column(db.String(30), nullable=False)
+    diameter = db.Column(db.Integer)  
+    climate = db.Column(db.String(80))
+    terrain = db.Column(db.String(80))
+    population = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<Planets %r>' % self.name  
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "diameter": self.diameter,
+            "climate": self.climate,
+            "terrain": self.terrain,
+            "population": self.population
+            
+        }  
+
+#TABLA NAVES
+class Starships(db.Model):
+    id = db.Column(db.Integer, primary_key=True) 
+    name = db.Column(db.String(30), nullable=False)
+    model = db.Column(db.String(80))
+    manufacturer = db.Column(db.String(80))
+    passengers = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<Starships %r>' % self.name 
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "model": self.model,
+            "manufacturer": self.manufacturer,
+            "passangers": self.passangers,
+ 
+            
+        }    
+         
